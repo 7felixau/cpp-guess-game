9b747cfd974b80b28a811db39c90def0ccf4f427
cyber-builder-65 <7@felixau.in>
2026-07-25 15:12:33 +0530

    Implement features in game.cpp - Version 4

--- game.cpp
+++ game.cpp
@@ -5,3 +5,10 @@
 int main() {
     std::srand(std::time(0));
     int secret = std::rand() % 100 + 1;
+    int guess = 0;
+    int attempts = 0;
+
+    std::cout << "Welcome to Number Guessing Game!" << std::endl;
+    std::cout << "Guess a number between 1 and 100." << std::endl;
+
+    while (guess != secret) {
